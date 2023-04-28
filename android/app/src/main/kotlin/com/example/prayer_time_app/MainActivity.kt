d9b51c9c663ebee2eb37cb0ef6739bbc0e7af3af
Abdullah Harits <harits.abdullah19@gmail.com>
2023-04-28 16:47:23 +0700

    implement widget

--- android/app/src/main/kotlin/com/example/prayer_time_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/prayer_time_app/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.prayer_time_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:prayer_time_app/home/home_screen.dart';
+import 'package:prayer_time_app/playground.dart';
 
+// TODO save cache to local storage
+// TODO implement widget
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
+}
+
+void mainPlayground() {
+  WidgetsFlutterBinding.ensureInitialized();
+  HomeWidget.registerBackgroundCallback(backgroundCallback);
+  runApp(const MyAppPlayground());
+}
+
+// Called when Doing Background Work initiated from Widget
+Future<void> backgroundCallback(Uri? uri) async {
+  if (uri?.host == 'updatecounter') {
+    int counter = 0;
+    await HomeWidget.getWidgetData<int>('_counter', defaultValue: 0)
+        .then((value) {
+      counter = value ?? 0;
+      counter++;
+    });
+    await HomeWidget.saveWidgetData<int>('_counter', counter);
+    await HomeWidget.updateWidget(
+        name: 'AppWidgetProvider', iOSName: 'AppWidgetProvider');
+  }
 }
 
 class MyApp extends StatelessWidget {
